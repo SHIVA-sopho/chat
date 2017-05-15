975b2b681d8eb5e1270cf134d546234a27b163b0
SHIVA-sopho <coolshiva.gandhi@gmail.com>
2017-05-15 23:18:02 +0530

    user connected / disconnected notification added

--- index.js
+++ index.js
@@ -13,20 +13,25 @@ var username = "user";
 
 IO.on('connection',function(socket){
 	console.log('a user is connected');
+	socket.broadcast.emit('connected', socket.username);
 	socket.on('disconnect',function(){
-		console.log('user disconnected');
-		socket.broadcast.emit('disconnected', username);
+		console.log(username + ' disconnected');
+		socket.broadcast.emit('disconnected', socket.username);
 
 	});
 	
-
-
-
+   // recives the message from client
 	socket.on('chat message',function(user,msg){
 		//console.log('message: '+msg);
  		username = user;
+ 		socket.username = user;
+ 		// broad casts the message to other clients
 		socket.broadcast.emit('chat message', user,msg);
 	});
+
+   // user typing broadcast
+  
+
 });
 
 http.listen(3000,function(){
---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+var $firstpage = $('#firstpage');
+
+var $mainpage = $('#mainpage');
+
+var $username = $('#name');
+
+var $messages = $('#messages');
+
+var $message_input = $('#m');  
+
+
+

6755fee06b4794b213bff38f5f85f4a5e3216b17
SHIVA-sopho <coolshiva.gandhi@gmail.com>
2017-05-16 18:33:21 +0530

    user name feature added

--- views/main.js
+++ views/main.js
@@ -1,5 +1,43 @@
 $(function () {	
+    console.log("javascript is running");
+    var $window = $('window');
+    var $firstpage = $('#firstpage');
+    var $name_input = $('#name_input');
+    var $mainpage = $('#mainpage');
+    var $messages = $('#messages'); // messages log
+    var $message_input = $('#message_input');//messages
     var socket = io();
+
+    var username ;
+
+function set_user()
+{
+  console.log("set_user called");
+  username = $name_input.val();
+  console.log($name_input.val());
+  if(username)
+  {
+    $firstpage.fadeOut();
+    $mainpage.show();
+  }
+  
+  socket.emit('new_user_connected',username);
+  $name_input.off('click');
+}
+
+    
+
+
+
+ $('#name_input').keypress(function(event){
+
+console.log("key down worked");
+  if(event.which === 13)
+  {
+    console.log("its an enter key");
+    set_user();
+  }
+ });
 
  // emits message when form is  submitted   
     $('form').submit(function(){
